aad619ea785ddcec5912a6909865b18980cb99ba
saurabh007007 <116506457+saurabh007007@users.noreply.github.com>
2024-10-21 21:43:00 +0530

    addded

--- server/index.js
+++ server/index.js
@@ -35,6 +35,24 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://crop-connect-nine.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(express.json());
 
