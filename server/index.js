cb74db12aea4cb01db7db5f08f4026dadc7829f4
saurabh007007 <116506457+saurabh007007@users.noreply.github.com>
2024-11-02 22:57:06 +0530

    adding some

--- server/index.js
+++ server/index.js
@@ -36,6 +36,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Set-Cookie"],
   })
 );
 
